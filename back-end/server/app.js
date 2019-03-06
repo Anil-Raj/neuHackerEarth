5c06ae78e5f48b1b4ca1e66d4b2ea675370f19a3
anil_g <anil.gangadharaiah@neudesic.com>
2019-03-07 01:22:36 +0530

    wrote post method to accept js code and return the output of this code

--- back-end/server/app.js
+++ back-end/server/app.js
@@ -1,25 +1,37 @@
-var http = require('http');
-var formidable = require('formidable');
+const express = require('express');
+const bodyParser = require('body-parser');
 var fs = require('fs');
+jsonFile = "temp.json";
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/prateek.nayan/anil-git/back-end/server/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(8080);
+const app = express();
+app.use(bodyParser.text({type:"*/*"}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+app.get('/', (req, res) => {
+    fs.readFile(jsonFile, (err,data)=>{
+    res.send(JSON.parse(data));
+    });
+    
+    
+});
+app.post('/', function(req, res) {
+  console.log(req.body);
+fs.writeFile('./bin/app.js', JSON.parse(req.body).code, function(err, data){
+  var exec = require('child_process').exec;
+  exec("node ./bin/app.js", function(err,stdout,stderr){
+        res.send(stdout);
+    });
+    if (err) console.log(err);
+    console.log("Successfully Written to File.");
+});
+});
+
+
+// listen for requests
+app.listen(3030, () => {
+    console.log("Server is listening on port 3030");
+});
