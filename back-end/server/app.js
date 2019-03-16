c1b4dbfd32e674bb62260f3a236f3b9805fd739f
anil_g <anil.gangadharaiah@neudesic.com>
2019-03-16 22:43:32 +0530

    added c# compiler option

--- back-end/server/app.js
+++ back-end/server/app.js
@@ -1,34 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var fs = require('fs');
 jsonFile = "temp.json";
+var compiler = require('./compiler') 
+var problem = require('./problem');
+var headers = require('./headers');
 
 const app = express();
 app.use(bodyParser.text({type:"*/*"}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+app.use(headers.cors);
 
-app.get('/', (req, res) => {
-    fs.readFile(jsonFile, (err,data)=>{
-    res.send(JSON.parse(data));
-    });
-    
-    
-});
-app.post('/', function(req, res) {
-  console.log(req.body);
-fs.writeFile('./bin/app.js', JSON.parse(req.body).code, function(err, data){
-  var exec = require('child_process').exec;
-  exec("node ./bin/app.js", function(err,stdout,stderr){
-        res.send(stdout);
-    });
-    if (err) console.log(err);
-    console.log("Successfully Written to File.");
-});
-});
+app.get('/problems', problem.problemsGetMethod);
+app.get('/problems/:id', problem.problemGetMethod);
+app.post('/', compiler.compilerPostMethod);
 
 
 // listen for requests
