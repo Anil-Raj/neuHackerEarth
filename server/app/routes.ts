cba16284ee153c66c3dd0002294e57b7a7bdda53
Anil G <anil.gangadharaiah@neudesic.com>
2019-05-06 01:51:12 +0530

    server simplified

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -1,19 +1,8 @@
 
-import * as compilerRouteHandler from './Components/Compiler/compilerRouteHandler';
-import * as challenges from './Components/Challenge/challenges';
-import * as codearena from './Components/CodeArena/codearenaHandler';
-import * as problem from './Components/Problem/problem';
-import * as code from './Components/CodeStore/code';
+import * as compilerRouteHandler from './compilerRouteHandler';
 import * as express from 'express';
 
 export function handler(app:express.Application){
-    app.get('/problems', problem.getProblemList);
-    app.get('/problems/:id', problem.problemGetMethod);
     app.post('/compile', compilerRouteHandler.compilerPostMethod);
-    app.get('/challenges', challenges.getAll);
-    app.post('/codearena/join_fight', codearena.join_fight);
-    // this.app.get('/codearena/isRingReady/:id', codearena.isRingReady);
-    app.post('/codearena/compile', codearena.compileUserCode);
-    app.post('/code', code.getCode);
 
 }
